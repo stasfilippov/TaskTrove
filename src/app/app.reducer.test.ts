7cdced572be5da1a99bb351c4106f9a69880d8f5
stasfilippov <kron888999@gmail.com>
2024-05-25 00:13:53 +0300

    -fix appSlice-test

--- src/app/app.reducer.test.ts
+++ src/app/app.reducer.test.ts
@@ -1,4 +1,7 @@
 import { appActions, AppInitialStateType, appSlice } from "app/appSlice";
+import { authThunks } from "features/auth/model/authSlice";
+import { tasksThunks } from "features/TodolistsList/model/tasks/tasksSlice";
+import { todolistsThunks } from "features/TodolistsList/model/todolist/todolistsSlice";
 
 let startState: AppInitialStateType;
 
@@ -15,7 +18,20 @@ test("correct error message should be set", () => {
   expect(endState.error).toBe("some error");
 });
 
-// test("correct status should be set", () => {
-//   const endState = appSlice(startState, appActions.setAppStatus({ status: "loading" }));
-//   expect(endState.status).toBe("loading");
+// test("test_state_loading_on_pending_action", () => {
+//   const action = { type: "app/somePendingAction" };
+
+//   const newState = appSlice(startState, action);
+//   expect(newState.status).toEqual("loading");
 // });
+test("test_reducer_ignores_specific_rejections", () => {
+  const rejectedActions = [
+    { type: todolistsThunks.addTodolist.rejected.type, payload: { messages: ["Error"] } },
+    { type: tasksThunks.addTask.rejected.type, payload: { messages: ["Error"] } },
+    { type: authThunks.initializeApp.rejected.type, payload: { messages: ["Error"] } },
+  ];
+  rejectedActions.forEach((action) => {
+    const state = appSlice(startState, action);
+    expect(state.error).toBeNull();
+  });
+});
